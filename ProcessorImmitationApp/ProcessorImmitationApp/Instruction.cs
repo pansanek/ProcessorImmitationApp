521ca82bd66f104fb2c3f3a090f0a08dbb334c57
SashaPotemkin <alex.potjomkin@gmail.com>
2024-10-13 14:45:54 +0300

    Класс для представления команды

--- ProcessorImmitationApp/ProcessorImmitationApp/Instruction.cs
+++ ProcessorImmitationApp/ProcessorImmitationApp/Instruction.cs
@@ -8,5 +8,20 @@ namespace ProcessorImmitationApp
 {
     internal class Instruction
     {
+        public int OpCode { get; }
+        public int Operand1 { get; }
+        public int Operand2 { get; }
+
+        public Instruction(int opCode, int operand1, int operand2)
+        {
+            OpCode = opCode;
+            Operand1 = operand1;
+            Operand2 = operand2;
+        }
+
+        public override string ToString()
+        {
+            return $"[{OpCode}, {Operand1}, {Operand2}]";
+        }
     }
 }
