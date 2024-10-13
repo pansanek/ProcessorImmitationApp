f4f7d864482f2cbc838cc207bf0b0e60c723ef9c
SashaPotemkin <alex.potjomkin@gmail.com>
2024-10-13 14:46:27 +0300

    Класс для управления памятью команд и данных

--- ProcessorImmitationApp/ProcessorImmitationApp/Memory.cs
+++ ProcessorImmitationApp/ProcessorImmitationApp/Memory.cs
@@ -8,5 +8,30 @@ namespace ProcessorImmitationApp
 {
     internal class Memory
     {
+        public int[] DataMemory { get; private set; }
+        public List<Instruction> CommandMemory { get; private set; }
+
+        public Memory(int dataSize)
+        {
+            DataMemory = new int[dataSize];
+            CommandMemory = new List<Instruction>();
+        }
+
+        public void LoadData(int[] initialData)
+        {
+            Array.Copy(initialData, DataMemory, initialData.Length);
+        }
+
+        public void LoadProgram(List<Instruction> program)
+        {
+            CommandMemory = program;
+        }
+
+        public Instruction FetchInstruction(int pc)
+        {
+            Instruction instruction = CommandMemory[pc];
+            Console.WriteLine($"Извлечена команда (IR): {instruction}");
+            return instruction;
+        }
     }
 }
