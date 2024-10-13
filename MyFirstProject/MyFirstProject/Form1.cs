d159e27a3da30b94932ad1cf21dfe0cfb6c1a1db
SashaPotemkin <alex.potjomkin@gmail.com>
2024-10-13 14:31:20 +0300

    Добавьте файлы проекта.

---
+++ MyFirstProject/MyFirstProject/Form1.cs
@@ -0,0 +1,21 @@
+namespace MyFirstProject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String text = textBox1.Text;
+            MessageBox.Show(text);
+        }
+    }
+}
